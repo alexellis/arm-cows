699c8f4e6f2533a7af33c23a282c09e79deaebd9
Alex Ellis <AlexEllis2@gmail.com>
2016-12-20 21:13:44 +0000

    Let's make this quicker

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
-FROM alexellis2/nodejs-armhf:6.9.2
+FROM armhf/alpine:latest
+RUN apk add --update nodejs
 ENTRYPOINT []
 COPY package.json .
 RUN npm i
--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.get('/', function (req, res) {
   }
 });
 
-var server = app.listen(80, function () {
+var server = app.listen(3000, function () {
   var port = server.address().port;
   console.log('Listening on port ', port);
 });
